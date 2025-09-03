4ba0badfdaeaadd89ee7a3b73714b1d500225956
Nicholas A. Coles <40498285+ColesNicholas@users.noreply.github.com>
2025-09-03 11:23:03 -0400

    Reviewed up to GlobalGratitude_final_cleaned
    Updated formatting along the way

--- code/Cleaned_Final_Dataset.R
+++ code/Cleaned_Final_Dataset.R
@@ -1,3 +1,6 @@
+## --------------------------------------------------------------------------------------
+# Developed by Annabel, reviewed by Nicholas
+## --------------------------------------------------------------------------------------
 library(tidyverse)
 library(here)
 
@@ -5,19 +8,34 @@ library(here)
 i_am("code/Cleaned_Final_Dataset.R")
 
 # fetch main survey data
-data_main <- readRDS(file = here("data", "GlobalGratitude_Final.Rds"))
+data_main <- 
+  readRDS(file = here("data", 
+                      "GlobalGratitude_Final.Rds")
+          )
+
 data_main <- data_main %>%
-  select(StartDate:pageNo)
+  select(StartDate : pageNo)
 
 # fetch the USA_02b (harmonized) survey data
-data_USA_02b <- read.csv(file = here("data", "USA_02b_raw_harmonized.csv"))
-data_USA_02b <- data_USA_02b %>% 
+data_USA_02b <- 
+  read.csv(file = here("data", 
+                       "USA_02b_raw_harmonized.csv")
+           )
+
+data_USA_02b <- 
+  data_USA_02b %>% 
   select(StartDate:pageNo) %>%
   rename("events_list" = "control_list") 
 
 # fetch the USA_02c survey data
-data_USA_02c <- read.csv(file = here("data", "USA_02c.csv"))
-data_USA_02c <- data_USA_02c %>% select(StartDate:pageNo)
+data_USA_02c <- 
+  read.csv(file = here("data", 
+                       "USA_02c.csv")
+           )
+
+data_USA_02c <- 
+  data_USA_02c %>% 
+  select(StartDate : pageNo)
 
 #match columns
 data_main <- data_main %>%
@@ -36,13 +54,21 @@ data_main <- data_main %>%
   }))
 
 # read and combine the CSV files
-data <- bind_rows(data_main, data_USA_02c)
-data <- bind_rows(data, data_USA_02b)
+data <- 
+  bind_rows(data_main, 
+            data_USA_02c)
+
+data <- 
+  bind_rows(data, 
+            data_USA_02b)
 
 #clean data
-data_cleaned <- data %>% 
+data_cleaned <- 
+  data %>% 
+  
   #removed test links
   filter(DistributionChannel != "preview",
+         
          # 4/22/2024 TUR_01 used real link for testing purposes
          ResponseId != "R_42KUGZSS76NgWH7",
          ResponseId != "R_4W4EXfgeyk1rCYF",
@@ -58,15 +84,19 @@ data_cleaned <- data %>%
          ResponseId != "R_8lQxsY2ITg7HKh1",         
          ResponseId != "R_8HXsI5PQftiJUYk",
          ResponseId != "R_8iVWI3CN49ACiUp",
+         
          #6/6/2024 Removed USA_01 duplicate data
          ResponseId != "R_6rDfD5u84z6WufT",
+         
          #11/21/2024 Removed DZA_01 test data
          ResponseId != "R_4ioYJK1zR2FgR4R",
          ResponseId != "R_4OvlyOmeTsmLpFn",
          ResponseId != "R_4BA1gbglSYnVyDK")
          
 #clean data
-data_cleaned <- data_cleaned %>%
+data_cleaned <- 
+  data_cleaned %>%
+  
   #removed incomplete surveys
   filter(consent == 1,
          Progress >= 95,
@@ -74,7 +104,6 @@ data_cleaned <- data_cleaned %>%
          condition_type != "NA",
          lab != "NA") %>% 
   select(StartDate:pageNo) #Remove non-relevant columns
-
 
 # change the 'incentive' column from "volunteer" to "paid" for NOR_01 participants after 11/19/2024
 data_cleaned <- data_cleaned %>%
@@ -84,17 +113,19 @@ data_cleaned <- data_cleaned %>%
 
 # fix age data where wrong info is inputted
 data_cleaned <- data_cleaned %>%
-  mutate(age = if_else(age > 99, 2024 - age, age))
+  mutate(age = if_else(age > 99, 
+                       2024 - age, 
+                       age))
 
 #rename interventions and controls
-data_cleaned <- data_cleaned %>% 
+data_cleaned <- 
+  data_cleaned %>% 
   mutate(condition = case_when(
     condition == "god.letter" ~ "divine.grat",
     condition == "hk.list"    ~ "naikan",
     condition == "sub"        ~ "mental.sub",
-    TRUE ~ condition
-  )
-  )
+    TRUE ~ condition)
+    )
 
 saveRDS(data_cleaned, 
         file = here('data',
--- code/USA_02b_Harmonize.R
+++ code/USA_02b_Harmonize.R
@@ -1,3 +1,6 @@
+## --------------------------------------------------------------------------------------
+# Developed by Annabel, reviewed by Nicholas
+## --------------------------------------------------------------------------------------
 library(tidyverse)
 library(here)
 
@@ -7,34 +10,43 @@ i_am("code/USA_02B_Harmonize.R")
 
 ## --------------------------------------------------------------------------------------
 # Fetch USA_02b survey
-data <- read.csv(file = here("data", 
-                             "USA_02b_raw_unharmonized.csv"))
+data <- 
+  read.csv(file = here("data", 
+                       "USA_02b_raw_unharmonized.csv")
+           )
 
 ## --------------------------------------------------------------------------------------
 
 # Change the condition column
-data <- data %>% mutate(condition = case_when(
-  condition == 1 ~ "measure",
-  condition == 2 ~ "events",
-  condition == 3 ~ "int.events",
-  condition == 5 ~ "list",
-  condition == 6 ~ "letter",
-  condition == 7 ~ "text",
-  condition == 8 ~ "hk.list",
-  condition == 9 ~ "sub",
-  condition == 11 ~ "god.letter",
-  TRUE ~ as.character(condition)
-))
+data <- 
+  data %>% 
+  mutate(condition = case_when(
+    condition == 1 ~ "measure",
+    condition == 2 ~ "events",
+    condition == 3 ~ "int.events",
+    condition == 5 ~ "list",
+    condition == 6 ~ "letter",
+    condition == 7 ~ "text",
+    condition == 8 ~ "hk.list",
+    condition == 9 ~ "sub",
+    condition == 11 ~ "god.letter",
+    TRUE ~ as.character(condition))
+    )
 
 # Change the condition_type column
-data <- data %>% mutate(condition_type = case_when(
-  condition_type == 1 ~ "control",
-  condition_type == 2 ~ "intervention",
-  TRUE ~ as.character(condition_type)
-))
+data <- 
+  data %>% 
+  mutate(condition_type = case_when(
+    condition_type == 1 ~ "control",
+    condition_type == 2 ~ "intervention",
+    TRUE ~ as.character(condition_type))
+    )
 
 # Remove the color columns (results are inaccurate due to survey error)
-data <- data %>% select(-color_task_red_6, -color_task_yellow_1, -color_task_blue_1)
+data <- 
+  data %>% 
+  select(-color_task_red_6, -color_task_yellow_1, 
+         -color_task_blue_1)
 
 # Save the processed data to CSV
 write.csv(data, 
